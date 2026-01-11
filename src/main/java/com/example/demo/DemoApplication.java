9d536202bf9a2c1a7d2a2d32ac3bdb057b5ae14d
Srija66 <dhulipallasrija66@gmail.com>
2026-01-10 20:55:35 -0600

    Added Controller and Service classes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -19,24 +19,5 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	private AtomicInteger counter = new AtomicInteger(0);
-	@GetMapping("/hello")
-	public Map<String, Object> hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		LocalTime time=LocalTime.now();
-		String greeting;
-		if (time.isBefore(LocalTime.NOON)) {
-			greeting = "Good morning";
-		} else if (time.isBefore(LocalTime.of(18, 0))) {
-			greeting = "Good afternoon";
-		} else {
-			greeting = "Good evening";
-		}
-		int visit=counter.incrementAndGet();
 
-		Map<String, Object> response = new LinkedHashMap<>();
-		response.put("greeting", greeting);
-		response.put("name", name);
-		response.put("visitorNumber", visit);
-		return response;
-	}
 }
