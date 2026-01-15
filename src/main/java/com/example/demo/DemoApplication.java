58eeeba118f962c103c039e22c466b1a60fe18c7
Srija Dhulipalla <65708365+Srija66@users.noreply.github.com>
2026-01-14 20:16:49 -0600

    Revert "Develop (#7)"
    This reverts commit fd6e2968f8f17dd182b875b92e15f688493d8819.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,16 @@
 
 package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.web.bind.annotation.GetMapping;
+		import org.springframework.web.bind.annotation.RequestParam;
+		import org.springframework.web.bind.annotation.RestController;
+		import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+		import java.time.LocalTime;
+		import java.util.HashMap;
+		import java.util.LinkedHashMap;
+		import java.util.Map;
+		import java.util.concurrent.atomic.AtomicInteger;
 
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
@@ -13,5 +19,24 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	private AtomicInteger counter = new AtomicInteger(0);
+	@GetMapping("/hello")
+	public Map<String, Object> hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		LocalTime time=LocalTime.now();
+		String greeting;
+		if (time.isBefore(LocalTime.NOON)) {
+			greeting = "Good morning";
+		} else if (time.isBefore(LocalTime.of(18, 0))) {
+			greeting = "Good afternoon";
+		} else {
+			greeting = "Good evening";
+		}
+		int visit=counter.incrementAndGet();
 
+		Map<String, Object> response = new LinkedHashMap<>();
+		response.put("greeting", greeting);
+		response.put("name", name);
+		response.put("visitorNumber", visit);
+		return response;
+	}
 }
